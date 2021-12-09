7fffc1cc1f18f6dc125c09a761ad65dc3edc53be
Tohit Shaikh <shaikhtohit95@gmail.com>
2021-12-09 21:29:33 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class Sum
+{
+    public static void main()
+    {
+        int x=10;
+        int y=20;
+        System.out.println("Welcome To The  Git Codes...");
+        System.out.println(x+y);
+    }
+}
